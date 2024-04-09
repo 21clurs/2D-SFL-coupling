3c8cfa8d1a2535e4a593afa0cb25a0a1e176bec1
Clara Kim <cj3kim@v1040-wn-rt-a-61-101.campus-dynamic.uwaterloo.ca>
2024-04-09 12:39:32 -0400

    adding code to output/show normal vectors if desired

--- sim/output.cpp
+++ sim/output.cpp
@@ -7,8 +7,11 @@ void outputFrame(Mesh& m, std::string filename){
     std::ofstream file("./out/"+filename);
     for (uint i=0; i<m.verts.size(); i++)
         file<<"v "<<m.verts[i][0]<<" "<<m.verts[i][1]<<std::endl;
-
     file<<std::endl;
+
+    /*for (uint i=0; i<m.verts.size(); i++)
+        file<<"vn "<<(m.calc_vertex_normal(i))[0]<<" "<<(m.calc_vertex_normal(i))[1]<<std::endl;
+    file<<std::endl;*/
     
     for (uint i=0; i<m.faces.size(); i++)
         file<<"f "<<m.faces[i][0]<<" "<<m.faces[i][1]<<std::endl;
