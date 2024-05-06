63ed40aaeef824caa5c6ec54aa1d2627ccb67549
Clara Kim <cj3kim@Claras-MacBook-Air.local>
2024-05-06 16:17:00 -0400

    cleaning up some comments

--- sim/sim.cpp
+++ sim/sim.cpp
@@ -51,23 +51,13 @@ void Sim::step_advect(){
 void Sim::step_HHD(){
     const std::vector<Eigen::Vector2d> quadrature_GQ = BoundaryIntegral::gaussian_quadrature();
     const std::vector<Eigen::Vector2d> quadrature_DE = BoundaryIntegral::tanh_sinh_quadrature();
-
-    //for(size_t i =0; i<quadrature_DE.size(); i++){
-    //    std::cout<<quadrature_DE[i].x()<<","<<quadrature_DE[i].y()<<std::endl;
-    //}
-
-    // i for outer -- when looping through verts, and then the faces adjacent to those verts
-    // j for inner
-
-    // iterate over vertices
-
+    
     const double negOneOver2pi = -1.0/(2*M_PI);
 
     std::vector<double> dPhidn(n, 0.0);
     std::vector<double> curlA(n, 0.0);
 
     for (size_t i=0; i<m.verts.size(); i++){
-        //std::cout<<"i"<<": "<<i<<std::endl;
         int xi_a, xi_b;
         // iterate over each adjacent edge (only 2 in this case)
         for (size_t p=0; p<2; p++){
@@ -75,10 +65,6 @@ void Sim::step_HHD(){
 
             Eigen::Vector2d n_x = m.calc_face_normal(f_i);
             Eigen::Vector2d t_x = m.calc_face_tangent(f_i);
-            /*if (i==0 ){
-                std::cout<<"f_i"<<": "<<f_i<<std::endl;
-                //std::cout<<"t_x"<<": "<<t_x<<std::endl;
-            }*/
             
             // shuffling around the endpoint indices
             // such that xi_a is the 'current' vertex
@@ -94,7 +80,7 @@ void Sim::step_HHD(){
             double charge = 0;
             double II_curlA = 0;
 
-            double jacobian = 1.0/2.0; //super sketch but ok lets see...
+            double jacobian = 1.0/2.0; // a little sketchy here but ok lets see...
 
             // iterate over quadrature points
             for (size_t qi=0; qi<quadrature_DE.size(); qi++){
@@ -105,10 +91,6 @@ void Sim::step_HHD(){
 
                 Eigen::Vector2d x = lin_interp(m.verts[xi_a], m.verts[xi_b], qik);
                 Eigen::Vector2d v_x = lin_interp(m.vels[xi_a], m.vels[xi_b], qik);
-                /*if (i==0 ){
-                    std::cout<<"quad_x"<<": "<<x<<std::endl;
-                }*/
-                //std::cout<<"quad_pt_v_"<<i+1<<": "<<v_x<<std::endl;
 
                 double I_gradPhi = 0;
                 double I_curlA = 0;
@@ -116,11 +98,6 @@ void Sim::step_HHD(){
                 // iterating over all faces (surface integral over the entire boundary)
                 for (size_t f_j=0; f_j<m.faces.size(); f_j++){
                     Eigen::Vector2d n_y = m.calc_face_normal(f_j);
-                    //Eigen::Vector2d t_y = m.calc_face_tangent(f_j);
-                    /*if (i==0 ){
-                        std::cout<<"f_j:"<<f_j<<std::endl;
-                        std::cout<<"n_y"<<": "<<n_y<<std::endl;
-                    }*/
 
                     double f_len = m.face_length(f_j);
 
@@ -144,18 +121,8 @@ void Sim::step_HHD(){
                                     val_curlA += jacobian * qjw * cross2d(n_y,v_y) * BoundaryIntegral::G(x,y) ;
                                 else if (p==1)
                                     val_curlA += jacobian * qjw * cross2d(v_y,n_y) * BoundaryIntegral::G(x,y) ;
-                                /*if (i==0 &&qi==0){
-                                    std::cout<<"y"<<": "<<y<<std::endl;
-                                    std::cout<<"val"<<": "<<cross2d(n_y,v_y) * BoundaryIntegral::G(x,y)<<std::endl;
-                                    std::cout<<"val_curlA * len"<<": "<<val_curlA * len<<std::endl;
-                                }*/
                             }
-                            I_curlA += val_curlA * len; // or f_len, like it is in my matlab?
-                            /*if (i==0 ){
-                                std::cout<<"SING_val_curlA"<<"_k_"<<k<<": "<<val_curlA<<std::endl;
-                                std::cout<<"SING_val_curlA * len"<<"_k_"<<k<<": "<< val_curlA * len<<std::endl;
-                                std::cout<<"SING_I_curlA"<<"_k_"<<k<<": "<< I_curlA <<std::endl;
-                            }*/
+                            I_curlA += val_curlA * len;
                         }
                     } else{
                         // gradPhi
@@ -174,11 +141,6 @@ void Sim::step_HHD(){
                             val_gradPhi += jacobian * qjw * v_y.dot(n_y) * dGdnx ;
                         }
                         I_gradPhi += val_gradPhi * f_len;
-                        /*if (i==0 ){
-                            std::cout<<"val_gradPhi"<<": "<<val_gradPhi<<std::endl;
-                            std::cout<<"val_gradPhi * len"<<": "<< val_gradPhi * f_len<<std::endl;
-                            std::cout<<"I_gradPhi"<<": "<< I_gradPhi <<std::endl;
-                        }*/
 
                         // curlA
                         double val_curlA = 0;
@@ -196,31 +158,16 @@ void Sim::step_HHD(){
                             val_curlA += jacobian * qjw * cross2d(n_y,v_y) * dGdtx ;
                         }
                         I_curlA += theta_i * val_curlA * f_len;
-                        //if (i==0 && qi==0)
-                            //std::cout<<"val_curlA_f_j"<<f_j<<": "<<val_curlA<<std::endl;
                     }
                 }
                 II_gradPhi += theta_i * jacobian * qiw * I_gradPhi;
-                II_curlA += jacobian * qiw*I_curlA;
-                /*if (i==0 ){
-                    std::cout<<"II_gradPhi"<<": "<< II_gradPhi <<std::endl;
-                }*/
                 charge += theta_i * jacobian * qiw * m.solid_angle(i)*v_x.dot(n_x);
+                II_curlA += jacobian * qiw*I_curlA;
             }
             dPhidn[i] += (II_gradPhi + charge)*m.face_length(f_i);
             curlA[i] += II_curlA*m.face_length(f_i);
-            /*if (i==0 ){
-                std::cout<<"dPhidn_i=0"<<": "<< dPhidn[i] <<std::endl;
-                std::cout<<"--------------------------" <<std::endl;
-            }*/
         }
     }
-
-    for(size_t i =0; i<n; i++){
-       //std::cout<<"dPhidn_"<<i+1<<": "<<dPhidn[i]<<std::endl;
-       //std::cout<<"curlA_"<<i+1<<": "<<curlA[i]<<std::endl;
-    }
-
     // constructing tangential projection
     std::vector<Eigen::Vector2d> V_t = std::vector<Eigen::Vector2d>(n,Eigen::Vector2d(0.0,0.0));
     std::vector<Eigen::Vector2d> V_n = std::vector<Eigen::Vector2d>(n,Eigen::Vector2d(0.0,0.0));
@@ -239,13 +186,6 @@ void Sim::step_HHD(){
     for (size_t i=0; i<m.verts.size(); i++){
         m.vels[i] = V_t[i] + V_n[i];
     }
-/*
-    for(size_t i =0; i<n; i++){
-       std::cout<<"dPhidn_"<<i+1<<": "<<dPhidn[i]<<std::endl;
-    }
-    for(size_t i =0; i<n; i++){
-       std::cout<<"curlA_"<<i+1<<": "<<curlA[i]<<std::endl;
-    }*/
 }
 
 void Sim::step_BEM(){
