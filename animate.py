d1005ffc33731ddeb91b824700f3ee95fba7ee4c
Clara Kim <cj3kim@v1040-wn-rt-a-35-123.campus-dynamic.uwaterloo.ca>
2024-06-05 11:05:20 -0400

    colouring points based on boundary type

--- animate.py
+++ animate.py
@@ -22,6 +22,7 @@ for frame in range(start, end):
     fList = []  # faces
     vnList = [] # vertex normals
     vtList = [] # vertex tangents
+    vColorList = [] # vertice colours
 
     if os.path.isfile(outdir+str(frame)+".txt"):
         with open(outdir+str(frame)+".txt","r") as curr_frame_file:
@@ -29,13 +30,23 @@ for frame in range(start, end):
                 r = row.split(" ")
                 if r[0] == 'v':
                     vList.append([ float(r[1]), float(r[2])])
+                    if len(r)>3:
+                        if "a" in r[3]:
+                            vColorList.append('#3ec1d5')
+                        elif "s" in r[3]:
+                            vColorList.append('#ff0070')
+                        elif "t" in r[3]:
+                            vColorList.append('#1b3481')
+                        else:
+                            vColorList.append('k')
+                    else:
+                        vColorList.append('k')
                 elif r[0] == 'f':
                     fList.append([ int(r[1]), int(r[2])])
                 elif r[0] == 'vn':
                     vnList.append([ float(r[1]), float(r[2])])
                 elif r[0] == 'vt':
                     vtList.append([ float(r[1]), float(r[2])])
-
         # why? I just like dealing with numpy :')
         # also, idk maybe one day will look into matplotlib .fill()
         v = np.array(vList)
@@ -47,11 +58,15 @@ for frame in range(start, end):
             [indexStart, indexEnd] = face
             xPts = np.array([v[indexStart,0], v[indexEnd,0]])
             yPts = np.array([v[indexStart,1], v[indexEnd,1]])
-            plt.plot(xPts, yPts, '#50a0c8')
+            faceColor = "#005668"
+            if(vColorList[indexStart] == 'k' or vColorList[indexEnd] == 'k' ):
+                faceColor = "#50a0c8"
+            plt.plot(xPts, yPts, faceColor)
 
         if "-showpoints" in sys.argv:
-            for vertex in v:
-                plt.plot(vertex[0], vertex[1],marker=".",color='#ff0070')
+            for j in range(len(v)):
+                vertex = v[j]
+                plt.plot(vertex[0], vertex[1],marker=".",color=vColorList[j])
         
         # set axes
         ax = plt.gca()
@@ -66,10 +81,12 @@ for frame in range(start, end):
             if "-showtangents" in sys.argv:
                 for i in range(len(vn)):
                     plt.arrow(v[i,0], v[i,1], vt[i,0]*0.2, vt[i,1]*0.2, head_width=.05, color="r")
+            if "-shownumpoints" in sys.argv:
+                plt.text(1.4, -1.9, "n: {}".format(len(v)), fontsize = 11)
             
         plt.text(-1.9, -1.9, "Frame: {}".format(frame), fontsize = 11)
 
-        # plt.show()
+        #plt.show()
         plt.savefig('./outFrames/frame-'+str(frame)+'.png', format="png")
         
         plt.clf()
